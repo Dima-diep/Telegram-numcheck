56605e727eb698832ec617adb0d1f83c4a43d0ba
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-30 00:13:54 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+api_id = ''  # Получить на https://my.telegram.org/auth
+api_hash = ''  # Получить на https://my.telegram.org/auth
+send_user = False  # Если задаём True, то для быстрого доступа контакт с пользователем отправится в раздел "Избранное"
